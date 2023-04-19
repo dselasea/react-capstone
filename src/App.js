3497d918232a219261e75091a1092fdd3db44dde
Daniel <dselasea@gmail.com>
2023-04-19 16:53:34 +0000

    add Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Display from './components/Display';
 import Details from './components/Details';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Display />} />
           <Route path="/info/:id" element={<Details />} />
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div style={{ color: '#fff' }}>
+    <h1>Crypto Currency Application</h1>
+  </div>
+);
+
+export default Header;
